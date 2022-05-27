ad9dd7e8a1f928c3d3fdaace81f2cf6e354bb401
purujit77 <purujit6351@gmail.com>
2022-05-27 18:47:15 +0530

    3rd update

--- index.js
+++ index.js
@@ -7,24 +7,28 @@ clapButton.addEventListener('click', (e)=>{
 
 var kick = new Audio("Kick.wav")
 var kickButton = document.getElementById('kick')
-kickButton.addEventListener('click', ()=>{
+kickButton.addEventListener('click', (e)=>{
+    e.preventDefault();
     kick.play();
 })
 
 var snare = new Audio("Snare.wav")
 var snareButton = document.getElementById('snare')
-snareButton.addEventListener('click', ()=>{
+snareButton.addEventListener('click', (e)=>{
+    e.preventDefault();
     snare.play();
 })
 
 var hihat = new Audio("Hi-hat.wav")
 var hihatButton = document.getElementById('hi-hat')
-hihatButton.addEventListener('click', ()=>{
+hihatButton.addEventListener('click', (e)=>{
+    e.preventDefault();
     hihat.play();
 })
 
 var cymbol = new Audio('Crash.wav')
 var cymbolButton = document.getElementById('cymbol')
-cymbolButton.addEventListener('click', ()=>{
+cymbolButton.addEventListener('click', (e)=>{
+    e.preventDefault();
     cymbol.play();
 })
